a985d7920edbf242c829694d685937b1e0e16b1e
pseudofool <saffroncoder@gmail.com>
2023-03-15 16:24:56 +0530

    fixed turn not changing

--- script.js
+++ script.js
@@ -71,8 +71,7 @@ reset.addEventListener('click', ()=>{
         element.innerText = "";
     });
     turn = "X";
-    document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
-    isgameover = true;
+    isgameover = false;
     document.getElementsByClassName('info')[0].innerText = "Turn for " + turn;
     document.querySelector(".imgbox").getElementsByTagName("img")[0].style.width = "0";
     // gameover.play();
